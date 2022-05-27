a3358785568dfb2ad27b67ae6300e557cfb47525
good.tony@yandex.ru <good.tony@yandex.ru>
2022-05-27 22:06:44 +0300

    Ugene and PhyMl3 Trees

--- phyl_analysis.sh
+++ phyl_analysis.sh
@@ -10,7 +10,7 @@ trimal -in srv_all_clust_alignment.fa -out srv_all_clust_aln_trim.fa
 # testin models using modeltest-ng
 modeltest-ng -i srv_all_clust_aln_trim.fa -o srv_all_clust_aln_trim_modeltest
 
+# looking for the best tree 
+raxml-ng --msa srv_all_clust_aln_trim.fa --model GTR+I+G4 --prefix srv_all_raxml --threads 2 --seed 222
 
-# running phyml
-phyml  -i srv_all_clust_aln_trim.fa -m 012345 -f m -v e -a e -c 4 -o tlr
 
